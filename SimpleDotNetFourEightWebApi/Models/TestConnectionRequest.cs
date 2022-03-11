342826f4953a916547eb03c9e819e5c46f79a929
sharpt00th <61414455+sharpt00th@users.noreply.github.com>
2022-03-11 07:29:51 +0000

    Initial commit of SimpleDotNetFourEightWebApi

---
+++ SimpleDotNetFourEightWebApi/Models/TestConnectionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleDotNetFourEightWebApi.Models
+{
+    public class TestConnectionRequest
+    {
+        [Required]
+        public string InstanceUrl { get; set; }
+
+        [Required]
+        public string ClientId { get; set; }
+
+        [Required]
+        public string ClientSecret { get; set; }
+    }
+}
